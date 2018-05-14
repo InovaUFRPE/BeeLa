30fb1f659ef95ad1255fc52fa58ded71c9b8d91f
Marcone Silva <marconemax42@gmail.com>
2018-05-14 15:13:59 -0300

    Melhorias de GUI - DatePicker e AlertDialog

--- app/src/main/java/com/beela/beela/Activity/CriarContaP2Activity.java
+++ app/src/main/java/com/beela/beela/Activity/CriarContaP2Activity.java
@@ -1,12 +1,19 @@
 package com.beela.beela.Activity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.beela.beela.DAO.Firebase;
@@ -23,8 +30,11 @@ import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Calendar;
+
 public class CriarContaP2Activity extends AppCompatActivity {
 
+    private static final String TAG = "CriarContap2Activity";
     private EditText editTextNome;
     private EditText editTextData;
     private EditText editTextGenero;
@@ -34,10 +44,21 @@ public class CriarContaP2Activity extends AppCompatActivity {
     private String senha;
     private FirebaseAuth autenticacao;
 
+
+    private TextView displayDate;
+    private DatePickerDialog.OnDateSetListener dateSetListener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criar_conta_p2);
+        datadata();
+
+
+
+
+
 
         final Bundle parametros = this.getIntent().getExtras();
 
@@ -115,6 +136,58 @@ public class CriarContaP2Activity extends AppCompatActivity {
         startActivity(abrirPerfil);
         finish();
     }
+
+
+    public void datadata(){
+        displayDate = (TextView) findViewById(R.id.textViewPedirData);
+
+        displayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Calendar calendar = Calendar.getInstance();
+                int ano = calendar.get(Calendar.YEAR);
+                int mes = calendar.get(Calendar.MONTH);
+                int dia = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        CriarContaP2Activity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        dateSetListener,
+                        ano, mes, dia);
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+
+            }
+
+
+
+
+
+        });
+
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+
+                Log.d(TAG, "dataSet: date:" + dayOfMonth + "/" + month + "/" + year  );
+            }
+
+
+
+
+        };
+
+
+
+    }
+
 
 
 }

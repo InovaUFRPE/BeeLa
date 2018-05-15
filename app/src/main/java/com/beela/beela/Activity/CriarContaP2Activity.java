e3b0a01861631af5c1dfe3ec5bf2f3df70006ea4
EbonyMarques <ebony.marquesr@ufrpe.br>
2018-05-15 19:45:58 -0300

    Adição de preferências na sessão.

--- app/src/main/java/com/beela/beela/Activity/CriarContaP2Activity.java
+++ app/src/main/java/com/beela/beela/Activity/CriarContaP2Activity.java
@@ -33,8 +33,6 @@ import com.google.firebase.auth.FirebaseUser;
 import java.util.Calendar;
 
 public class CriarContaP2Activity extends AppCompatActivity {
-
-
     private EditText editTextEmail;
     private EditText editTextSenha;
 
@@ -45,17 +43,10 @@ public class CriarContaP2Activity extends AppCompatActivity {
     private String data;
     private String genero;
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_criar_conta_pt2);
-
-
 
         final Bundle parametros = this.getIntent().getExtras();
 

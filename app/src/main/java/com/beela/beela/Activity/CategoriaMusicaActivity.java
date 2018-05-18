2eeab808ff8b35a29ccc902caa9a401bca312f9b
Unknown <pamella.lesilvao@gmail.com>
2018-05-17 21:25:48 -0300

    Atividade #982 e #984 Tela Criar Perfil e Tela visualizar Perfil

---
+++ app/src/main/java/com/beela/beela/Activity/CategoriaMusicaActivity.java
@@ -0,0 +1,13 @@
+package com.beela.beela.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategoriaMusicaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categoria_musica);
+    }
+}
